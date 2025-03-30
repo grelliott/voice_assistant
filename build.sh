142b1f731c794bdefd4d4d04cc3e017552d052ef
Grant Elliott <grant@grantelliott.ca>
2025-03-30 11:18:23 -0400

    Update dependency versioning

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #  conan profile detect --force
-conan install -s build_type=Release . --output-folder=conan --build=missing
+command -v conan || (echo "Please install requirements" && exit 1)
+conan install --update -s build_type=Release . --output-folder=conan --build=missing
 
 cmake -S . -B build -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=conan/build/Release/generators/conan_toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
--- include/version.hpp
+++ include/version.hpp
@@ -14,9 +14,9 @@
 #ifndef VERSION_HPP
 #define VERSION_HPP
 
-constexpr auto APP_VERSION {"0.0.1"};
+constexpr auto APP_VERSION {"0.0.2"};
 constexpr auto APP_DESCRIPTION {"Voice assistant application"};
-constexpr auto COMMIT_ID {"1aabdd65791a02b819e9ce5e556e8ad1f49550f6"};
+constexpr auto COMMIT_ID {"051472be89784b99672ffc78fa38ec72611542ea"};
 constexpr auto BRANCH_NAME {"main"};
 
 #endif
